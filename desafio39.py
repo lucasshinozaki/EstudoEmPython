f598ed6da3f7ad10b81f4ca685b0c5736c14c972
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-07 18:31:31 -0300

    Att desafio 39 ano_atual

--- desafio39.py
+++ desafio39.py
@@ -2,13 +2,11 @@ from datetime import date
 
 ano = int(input('Ano de nascimento: '))
 
-ano_atual = date.today()
+ano_atual = date.today().year
 
-if (ano_atual.year - ano) == 18:
+if (ano_atual - ano) == 18:
     print('Voce tem que se alistar esse ano')
-elif (ano_atual.year - ano) < 18:
-    print('Voce ainda vai se alistar: falta {} anos'.format(18 - (ano_atual.year - ano)))
+elif (ano_atual - ano) < 18:
+    print('Voce ainda vai se alistar: falta {} anos'.format(18 - (ano_atual - ano)))
 else:
-    print('Voce ja tinha que ter se alistador: {} anos se passaram'.format((ano_atual.year - ano) - 18))
-
-
+    print('Voce ja tinha que ter se alistador: {} anos se passaram'.format((ano_atual - ano) - 18))
