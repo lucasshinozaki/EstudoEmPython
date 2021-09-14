fecd69cf30b0ebbdddacd20f5db5773445c332c2
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-14 18:45:52 -0300

    Desafio 55 Maior e menor da sêquencia

--- desafio55.py
+++ desafio55.py
@@ -1,16 +1,18 @@
 #Desafio 55
 #Faça um programa que leia o peso de cinco pessoas
 #No final, mostre qual foi o maior e menor peso lidos
-
 maior = 0
-menor = 1000
-
+menor = 0
 for c in range(0, 5):
     peso = float(input('Digite seu peso: '))
-    if(peso > maior):
+    if c == 1:
         maior = peso
-    elif(menor < peso):
         menor = peso
+    else:
+        if(peso > maior):
+            maior = peso
+        elif(menor < peso):
+            menor = peso
 
 print("O maior peso: {}".format(maior))
 print("O menor peso: {}".format(menor))
