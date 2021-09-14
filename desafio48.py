6a4ff5393fe47eace38383be9e9c264e4068c117
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-14 17:50:19 -0300

    Desafio 48 Soma impares multiplos de tres

--- desafio48.py
+++ desafio48.py
@@ -1,5 +1,10 @@
+#Desafio 48
+#faça um programa que calcule a soma entre todos os números impares
+#Que são muúltiplos de tre
+#E que se encontram no intervalo de 1 até 500
+
 soma = 0
-for c in range(1, 500):
+for c in range(1, 501, 2):
     if c % 3 == 0:
        soma += c
 
