e0152ebce01c7cbcf41cb26af0eb53777521644d
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-16 18:10:57 -0300

    Desafio 57 Validação de entrada att

--- desafio57.py
+++ desafio57.py
@@ -2,7 +2,8 @@
 #Faça um programa que leia o sexo de uma pessoa
 #Mas só aceite os valores 'M' ou 'F'.
 #Caso esteja erradp, peça a digitação novamente até te um valor correto.
-sexo = 'a'
-while sexo != 'M' and sexo != 'F':
-    sexo = input('Digite o seu sexo [F/M]: ')
 
+sexo = 'a'
+
+while sexo != 'M' and sexo != 'F':
+    sexo = input('Digite o seu sexo [F/M]: ').strip().upper()
