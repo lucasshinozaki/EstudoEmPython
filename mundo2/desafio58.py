02faedc9744007c7d0c593f55da9503ad5afde4a
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-23 17:42:57 -0300

    Desafio 58 Jogo da Adivinhação v2.0

--- mundo2/desafio58.py
+++ mundo2/desafio58.py
@@ -1,19 +1,24 @@
-#Melhore o Desafio 28
-#Onde o computador vai "pensar" em um número entre 0 e 10.
-#Só que agora o jogador vai tentar adivinhar até acertar
-#Mostrando no final quantos palpites forma necessários para vencer.
+# Melhore o Desafio 28
+# Onde o computador vai "pensar" em um número entre 0 e 10.
+# Só que agora o jogador vai tentar adivinhar até acertar
+# Mostrando no final quantos palpites forma necessários para vencer.
 
 import random
 
 num = random.randint(0, 10)
-print(num);
-
-n = 11
+print(num)
 cont = 0
-
-while num != n:
+acertou = False
+while not acertou:
     n = int(input('Digite um numero entre 0 a 10: '))
     cont += 1
+    if n == num:
+        acertou = True
+    else:
+        if n < num:
+            print("Mais... Tente novamente")
+        else:
+            print("Menos.. Tente novamente")
 
 print('Parabens voce venceu!!')
 print('QUantidade de tentativas: {}'.format(cont))
