03c0fa153a503145b3023ad04adb7549f338e19a
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-30 18:17:01 -0300

    Aula16 - Tuplas

---
+++ mundo3/aula16.py
@@ -0,0 +1,13 @@
+#Tuplas
+
+lanche = ('Hamburque', 'Suco', 'Pizza', 'Pudim')
+print(lanche[::-1])
+
+for cont in range(0, len(lanche)):
+    print(f'Eu vou comer {lanche[cont]}')
+
+for pos, comida in enumerate(lanche):
+    print(f'Posição {pos} Eu vou comer {comida}')
+
+
+print('Comi pra caramba !!!')
